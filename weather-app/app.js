930ae080130eebe1373d02cf07b8ab98e54707bd
capKloss72 <tym.zon@gmail.com>
2021-05-16 20:16:04 +0800

    Started on getting information out of the geolocation service, still getting errors on return from the response query

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,20 @@
 const request = require('postman-request');
+const { CLIENT_RENEG_LIMIT } = require('tls');
 
-const urlWS =
-  'http://api.weatherstack.com/current?access_key=e3bddab3e02266169155bdb5d371c3d1&query=--31.953512,115.857048';
+const urlMapbox =
+  'https://api.mapbox.com/geocoding/v5/mapbox.places/Perth.json?access_token=pk.eyJ1IjoiY2Fwa2xvc3M3MiIsImEiOiJja29yNDBtaGgwaTh1MndwZGVsYzJzc3IxIn0.35UjY6fbuUT8aReyX33GZw';
 
-request({ url: urlWS }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current.pressure);
+const center = request({ url: urlMapbox, json: true }, (error, response) => {
+  console.log(`${response.body.features[0].center[1]}`);
+  return response.body.features[0].center;
+});
+
+console.log(`${center[1]},${center[0]}`);
+
+let urlWS = `http://api.weatherstack.com/current?access_key=e3bddab3e02266169155bdb5d371c3d1&query=${center[1]},${center[0]}&units=m`;
+
+request({ url: urlWS, json: true }, (error, response) => {
+  console.log(
+    `${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees and it feels like ${response.body.current.feelslike} degrees`
+  );
 });

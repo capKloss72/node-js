6ad14abe10bdb0a2fc9aa4feeea79e2e4dcd09af
Tym Zon <tym.zon@gmail.com>
2021-05-19 15:07:32 +0800

    Added request functioanlity for Mapbox and added defensive statements to catch no-connection and bad query errors

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,23 +1,33 @@
 const request = require('postman-request');
-const { CLIENT_RENEG_LIMIT } = require('tls');
+
+//let urlWS = `http://api.weatherstack.com/current?access_key=e3bddab3e02266169155bdb5d371c3d1&query=-31.9527,115.8605&units=m`;
+
+// let urlWS = `http://api.weatherstack.com/current?access_key=e3bddab3e02266169155bdb5d371c3d1&query=`;
+
+// request({ url: urlWS, json: true }, (error, response) => {
+//   if (error) {
+//     console.log('Unable to connect to weather service ice');
+//   } else if (response.body.success === false) {
+//     console.log(`${response.body.error.info}`);
+//   } else {
+//     console.log(
+//       `${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees and it feels like ${response.body.current.feelslike} degrees`
+//     );
+//   }
+// });
 
 const urlMapbox =
-  'https://api.mapbox.com/geocoding/v5/mapbox.places/Perth.json?access_token=pk.eyJ1IjoiY2Fwa2xvc3M3MiIsImEiOiJja29yNDBtaGgwaTh1MndwZGVsYzJzc3IxIn0.35UjY6fbuUT8aReyX33GZw';
-
-let newLoc = [];
+  'https://api.mapbox.com/geocoding/v5/mapbox.places/Perth.json?access_token=pk.eyJ1IjoiY2Fwa2xvc3M3MiIsImEiOiJja29yNDBtaGgwaTh1MndwZGVsYzJzc3IxIn0.35UjY6fbuUT8aReyX33GZw&limit=1';
 
 request({ url: urlMapbox, json: true }, (error, response) => {
-  console.log(`${response.body.features[0].center[1]}`);
-  newLoc = response.body.features[0].center;
-  console.log(newLoc);
-});
-
-console.log(`${newLoc[1]},${newLoc[0]}`);
-
-let urlWS = `http://api.weatherstack.com/current?access_key=e3bddab3e02266169155bdb5d371c3d1&query=${newLoc[1]},${newLoc[0]}&units=m`;
-
-request({ url: urlWS, json: true }, (error, response) => {
-  console.log(
-    `${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees and it feels like ${response.body.current.feelslike} degrees`
-  );
+  if (error) {
+    console.log('Unable to connect to mapping service ice');
+  } else if (response.body.features.length === 0) {
+    console.log(`Could not locate: ${response.body.query}`);
+  } else {
+    const latitude = response.body.features[0].center[1];
+    const Longitude = response.body.features[0].center[0];
+    console.log(`${latitude}, ${Longitude}`);
+    console.log(`${response.body.features[0].place_name}`);
+  }
 });

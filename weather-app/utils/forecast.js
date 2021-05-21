83d466f436218a9862fe11b7d38578ea6eafbae5
Tym Zon <tym.zon@gmail.com>
2021-05-21 15:23:46 +0800

    Added descrtusturing refactors

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -1,19 +1,19 @@
 const request = require('postman-request');
 
-const forecast = (location, callback) => {
+const forecast = ({ Latitude, Longitude }, callback) => {
   let url = `http://api.weatherstack.com/current?access_key=e3bddab3e02266169155bdb5d371c3d1&query=${encodeURIComponent(
-    location.Latitude
-  )},${encodeURIComponent(location.Longitude)}&units=m`;
+    Latitude
+  )},${encodeURIComponent(Longitude)}&units=m`;
 
-  request({ url: url, json: true }, (error, response) => {
+  request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback('Unable to connect to weather service ice', undefined);
-    } else if (response.body.success === false) {
-      callback(`${response.body.error.info}`), undefined;
+    } else if (body.success === false) {
+      callback(`${body.error.info}`), undefined;
     } else {
       callback(
         undefined,
-        `${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature} degrees and it feels like ${response.body.current.feelslike} degrees`
+        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees and it feels like ${body.current.feelslike} degrees`
       );
     }
   });

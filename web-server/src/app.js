f08cd2fa9fd5aa564e411a35733994774f0640e2
capKloss72 <tym.zon@gmail.com>
2021-05-21 18:07:32 +0800

    Removed static pages and replaced with hbs pages. Added partials. Updated paths to partials folder

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,14 +1,40 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
+// Define paths for express config
+const publicPath = path.join(__dirname, '../public');
+const hbsTemplatePath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// HBS engine and views locations
 app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, '../public')));
+app.set('views', hbsTemplatePath);
+hbs.registerPartials(partialsPath);
+
+// Path to static / public assets
+app.use(express.static(publicPath));
 
 app.get('', (req, res) => {
   res.render('index', {
     title: 'Weather App',
+    name: 'Tym Zon',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Me!',
+    name: 'Tym Zon',
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help!',
+    message: 'Example message!',
     name: 'Tym Zon',
   });
 });

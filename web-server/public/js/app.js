9b0ba7633b15818864a62c0f23fe349a5884c525
capKloss72 <tym.zon@gmail.com>
2021-05-22 19:21:55 +0800

    Added fetch method to get weather data from a url in clinet side JS

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,17 @@
 console.log('Client side JS is loaded');
+
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+//   response.json().then((data) => {
+//     console.log(data);
+//   });
+// });
+
+fetch('http://localhost:3000/weather?address=Perth').then((response) => {
+  response.json().then((data) => {
+    if (!data[0].address) {
+      return console.log({ error: 'You must provide a valid address!' });
+    }
+    console.log(data[0].location);
+    console.log(data[0].forecast);
+  });
+});

0d72f77f2e1820c245f2fac5975013ebb644b77c
capKloss72 <tym.zon@gmail.com>
2021-05-22 21:44:59 +0800

    Commiting to see if SSH work well

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,15 +1,5 @@
 console.log('Client side JS is loaded');
 
-// fetch('http://localhost:3000/weather?address=Perth').then((response) => {
-//   response.json().then((data) => {
-//     if (!data[0].address) {
-//       return console.log({ error: 'You must provide a valid address!' });
-//     }
-//     console.log(data[0].location);
-//     console.log(data[0].forecast);
-//   });
-// });
-
 const weatherForm = document.querySelector('form');
 const search = document.getElementById('search-input');
 const locationMsg = document.querySelector('#location');

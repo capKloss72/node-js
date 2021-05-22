67092ed76b73e2ab8c5e02bdb70285e5d8d4b243
capKloss72 <tym.zon@gmail.com>
2021-05-22 20:26:16 +0800

    Added fetch functionality to submit and input form on index.html. Added styling to the front page through css

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,17 +1,38 @@
 console.log('Client side JS is loaded');
 
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+// fetch('http://localhost:3000/weather?address=Perth').then((response) => {
 //   response.json().then((data) => {
-//     console.log(data);
+//     if (!data[0].address) {
+//       return console.log({ error: 'You must provide a valid address!' });
+//     }
+//     console.log(data[0].location);
+//     console.log(data[0].forecast);
 //   });
 // });
 
-fetch('http://localhost:3000/weather?address=Perth').then((response) => {
-  response.json().then((data) => {
-    if (!data[0].address) {
-      return console.log({ error: 'You must provide a valid address!' });
+const weatherForm = document.querySelector('form');
+const search = document.getElementById('search-input');
+const locationMsg = document.querySelector('#location');
+const forecastMsg = document.querySelector('#forecast');
+
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (!search.value) {
+    return (locationMsg.innerHTML = 'Search term must be provided!');
+  }
+  locationMsg.innerHTML = '';
+  forecastMsg.innerHTML = '';
+  locationMsg.innerHTML = 'Loading Message';
+  fetch(`http://localhost:3000/weather?address=${search.value}`).then(
+    (response) => {
+      response.json().then((data) => {
+        if (!data[0]) {
+          return (locationMsg.innerHTML = 'You must provide a valid address!');
+        }
+        locationMsg.innerHTML = data[0].location;
+        forecastMsg.innerHTML = data[0].forecast;
+      });
     }
-    console.log(data[0].location);
-    console.log(data[0].forecast);
-  });
+  );
+  console.log(search.value);
 });
